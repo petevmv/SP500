c8ae2d0e1b8ec000bcda0cb083a52b5a435fc3c9
Maksim Petev <petevmv@gmail.com>
2020-12-11 03:07:34 +0200

    adding more simple code

--- SP500_task.py
+++ SP500_task.py
@@ -1,23 +1,9 @@
-fileh = open("SP500.txt")
-lines = fileh.readlines()
-
-closing_idx_lst = []
-intrest_rate = []
-for row in lines[6:18]:
-    vals = row.strip().split(',')
-    closing_idx_lst.append(vals[1])
-    intrest_rate.append(vals[5])
-
-mean_SP = sum(map(float, closing_idx_lst))/len(closing_idx_lst)
-
-print(mean_SP)
-
-intrest_rate_float = []
-for el in  intrest_rate:
-    intrest_rate_float.append(float(el))
-
-max_interest = 0
-for f_el in intrest_rate_float:
-        if f_el > max_interest:
-            max_interest = f_el
-print(max_interest)
+file_handle = open("SP500.txt")
+lines = file_handle.readlines()
+SPclose = []
+interest = []
+for line in lines[6:18]:
+    SPclose.append(float(line.split(',')[1]))
+    interest.append(float(line.split(',')[5]))
+mean_SP = sum(SPclose)/len(SPclose)
+max_interest = max(interest)
